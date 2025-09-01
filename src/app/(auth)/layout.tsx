0c3520d93190ac060bd4e30a8c2bbe6657d8b275
AshirAzeem-Developer <ashir.dzynr@gmail.com>
2025-09-01 14:29:46 +0500

    Implementation of active links

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,39 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navLinks = [
+    { name: "Login", href: "/login" },
+    { name: "register", href: "/register" },
+    { name: "forgot-password", href: "/forgot-password" },
+  ];
+
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body className={`antialiased`}>
         {children}
+
+        {navLinks.map((link) => {
+          const isActive =
+            pathname === link.href ||
+            (pathname.startsWith(link.href) && link.href !== "/");
+          return (
+            <Link
+              className={isActive ? "font-bold mr-4" : "mr-4"}
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+
         <h1 className="bg-amber-200 text-6xl text-red-700">
           this is the Auth layout
         </h1>

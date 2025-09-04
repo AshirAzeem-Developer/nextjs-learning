c0e3e95062a93aa1daacca07e5371840200b3858
AshirAzeem-Developer <ashir.dzynr@gmail.com>
2025-09-04 11:34:30 +0500

    Implementation of Error Handling in  RootLayout  and Layouts
    1. Layouts k case mein adjacent error.tsx file error catch nahin karaygi magar adjacent page.tsx ka error catch hongay .
    
    2. agar root layout mein catch karna ha to hamayn global-error.tsx ka use karna hoga jo ka root layouts ya app ka andar kahin bhi agar error handle nahin hua to usay show karayga .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import "./globals.css";
+import ErrorWrapper from "./error-wrapper";
 export const metadata: Metadata = {
   title: {
     default: "Layout Meta Title ",
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <ErrorWrapper>{children}</ErrorWrapper>
+      </body>
     </html>
   );
 }
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,5 @@
 // import { notFound } from "next/navigation";
-"use client";
+// "use client";
 
 import { notFound } from "next/navigation";
 import React from "react";
@@ -11,10 +11,10 @@ const ReviewDetails = async ({
 }) => {
   const { reviewId, productId } = await params;
 
-  const random = Math.floor(Math.random() * 2);
-  if (random === 1) {
-    throw new Error("Error loading review");
-  }
+  // const random = Math.floor(Math.random() * 2);
+  // if (random === 1) {
+  //   throw new Error("Error loading review");
+  // }
   if (parseInt(reviewId) > 1000) {
     return notFound();
   }

7d201676ab1074d81e3f98cb955a562dcd655680
AshirAzeem-Developer <ashir.dzynr@gmail.com>
2025-09-04 10:14:55 +0500

    Implementation of Error.tsx and Handling Nested Routes Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,13 +1,28 @@
 "use client";
 
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { startTransition } from "react";
 
 type Props = {
   error: Error;
+  reset: () => void;
 };
 
-const ErrorBoundary = ({ error }: Props) => {
-  return <div>{error.message}</div>;
+const ErrorBoundary = ({ error, reset }: Props) => {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
+  return (
+    <div>
+      {error.message}
+      <button onClick={reload}>Reload</button>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,7 @@
 // import { notFound } from "next/navigation";
 "use client";
 
+import { notFound } from "next/navigation";
 import React from "react";
 
 const ReviewDetails = async ({
@@ -9,9 +10,13 @@ const ReviewDetails = async ({
   params: Promise<{ reviewId: string; productId: string }>;
 }) => {
   const { reviewId, productId } = await params;
+
+  const random = Math.floor(Math.random() * 2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
   if (parseInt(reviewId) > 1000) {
-    // return notFound();
-    throw new Error("Id Should be Less than 1000");
+    return notFound();
   }
   return (
     <div>
